4db9ef55b81c3ea12925df5d54e7f6a68e41499e
Konstantin <kfarr2@pdx.edu>
2016-01-01 08:04:32 -0800

    update copyright stuff

--- index.php
+++ index.php
@@ -6,5 +6,5 @@
 <iframe id="gameframe" src="gameframe.php" scrolling="no" frameborder="0"></iframe>
 <div id="instructions" class="text-center">WASD to move. Point &amp; click to shoot.</div>
 <script type="text/javascript" src="http://konstantinfarrell.github.io/static/js/credit.js"></script>
-<div class="text-center text-muted">Copyright 2015</div>
+<div class="text-center text-muted">Copyright 2016</div>
 <?php include('libs/footer.php'); ?>
