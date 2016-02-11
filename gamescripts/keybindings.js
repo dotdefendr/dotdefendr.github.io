6499962326b981a97e9810ce7757d9f1f4f12c63
Konstantin <kfarr2@pdx.edu>
2016-02-11 03:03:30 -0800

    Add pause function

--- gamescripts/keybindings.js
+++ gamescripts/keybindings.js
@@ -6,6 +6,7 @@
 
 // Handle special keys being pushed
 $(document).keydown(function(e){
+
     if(gameOver){
         switch(e.keyCode){
             case 13: // enter
@@ -15,6 +16,23 @@ $(document).keydown(function(e){
     }
     if(e.keyCode == 27 || e.which == 27){ // Escape was pressed
         restartGame();
+    }
+    if(e.keyCode == 80){
+        if(PAUSED == true){
+            $.playground().resumeGame(function(){});
+            $("#paused-screen").fadeOut(100);
+            $("#paused-screen").remove();
+            $("#playground").remove("#paused-screen");
+            PAUSED = false;
+        } else {
+            $.playground().pauseGame(function(){});
+            $("#playground").append("<div id='paused-screen'></div>");
+            $("#paused-screen").height(PLAYGROUND_HEIGHT);
+            $("#paused-screen").width(PLAYGROUND_WIDTH);
+            $("#paused-screen").append("<div class='text-center'><h1>PAUSED</h1></div>");
+            $("#paused-screen").fadeIn(100);
+            PAUSED = true;
+        }
     }
 });
 
